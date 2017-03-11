6648e67cd26163b6ce61e94bfaf7a3d71352614c
Dexaran <dexaran820@gmail.com>
2017-03-11 23:41:07 +0400

    Update ERC23_token.sol

--- ERC23_token.sol
+++ ERC23_token.sol
@@ -29,7 +29,7 @@ contract ERC23Token is ERC23 {
   
   
 // A function that is called when a user or another contract wants to transfer funds
-  function transfer(address _to, uint _value, bytes _data) returns (bool success) {
+  function transfer(address _to, uint _value) returns (bool success) {
      //filtering if the target is a contract with bytecode inside it
     if(isContract(_to))
     {
