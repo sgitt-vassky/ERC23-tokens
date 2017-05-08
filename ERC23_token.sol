024cb58019331c378f24804919dc1451018052b0
Dexaran <dexaran820@gmail.com>
2017-05-08 22:18:06 +0400

    Update ERC23_token.sol

--- ERC23_token.sol
+++ ERC23_token.sol
@@ -1,5 +1,14 @@
 pragma solidity ^0.4.9;
 
+ /* Receiver must implement this function to receive tokens
+ *  otherwise token transaction will fail
+ */
+ 
+ contract ContractReceiver {
+    function tokenFallback(address _from, uint _value, bytes _data){
+      //Incoming transaction code here
+    }
+}
  
  /* New ERC23 contract interface */
 
@@ -9,14 +18,16 @@ contract ERC23 {
   function allowance(address owner, address spender) constant returns (uint);
 
   function transfer(address to, uint value) returns (bool ok);
+  function transfer(address to, uint value, bytes data) returns (bool ok);
   function transferFrom(address from, address to, uint value) returns (bool ok);
   function approve(address spender, uint value) returns (bool ok);
   event Transfer(address indexed from, address indexed to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value, bytes data);
   event Approval(address indexed owner, address indexed spender, uint value);
 }
 
 
- /*
+ /**
  * ERC23 token by Dexaran
  *
  * https://github.com/Dexaran/ERC23-tokens
@@ -26,37 +37,51 @@ contract ERC23Token is ERC23 {
 
   mapping(address => uint) balances;
   mapping (address => mapping (address => uint)) allowed;
+
+  //function that is called when a user or another contract wants to transfer funds
+  function transfer(address _to, uint _value, bytes _data) returns (bool success) {
   
-  
-// A function that is called when a user or another contract wants to transfer funds
-  function transfer(address _to, uint _value) returns (bool success) {
-     //filtering if the target is a contract with bytecode inside it
-    if(isContract(_to))
-    {
-        transferToContract(_to, _value);
+    //filtering if the target is a contract with bytecode inside it
+    if(isContract(_to)) {
+        transferToContract(_to, _value, _data);
     }
-    else
-    {
-        transferToAddress(_to, _value);
+    else {
+        transferToAddress(_to, _value, _data);
+    }
+    return true;
+  }
+  
+  function transfer(address _to, uint _value) returns (bool success) {
+      
+    //standard function transfer similar to ERC20 transfer with no _data
+    //added due to backwards compatibility reasons
+    bytes memory empty;
+    if(isContract(_to)) {
+        transferToContract(_to, _value, empty);
+    }
+    else {
+        transferToAddress(_to, _value, empty);
     }
     return true;
   }
 
-//function that is called when transaction target is an address
-  function transferToAddress(address _to, uint _value) private returns (bool success) {
+  //function that is called when transaction target is an address
+  function transferToAddress(address _to, uint _value, bytes _data) private returns (bool success) {
     balances[msg.sender] -= _value;
     balances[_to] += _value;
     Transfer(msg.sender, _to, _value);
+    Transfer(msg.sender, _to, _value, _data);
     return true;
   }
   
-//function that is called when transaction target is a contract
-  function transferToContract(address _to, uint _value) private returns (bool success) {
+  //function that is called when transaction target is a contract
+  function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
     balances[msg.sender] -= _value;
     balances[_to] += _value;
-    contractReciever reciever = contractReciever(_to);
-    reciever.tokenFallback(msg.sender, _value);
+    ContractReceiver reciever = ContractReceiver(_to);
+    reciever.tokenFallback(msg.sender, _value, _data);
     Transfer(msg.sender, _to, _value);
+    Transfer(msg.sender, _to, _value, _data);
     return true;
   }
   
@@ -64,24 +89,21 @@ contract ERC23Token is ERC23 {
   function isContract(address _addr) private returns (bool is_contract) {
       uint length;
       assembly {
-            // retrieve the size of the code on target address, this needs assembly
+            //retrieve the size of the code on target address, this needs assembly
             length := extcodesize(_addr)
         }
-        if(length>0)
-        {
+        if(length>0) {
             return true;
         }
-        else
-        {
+        else {
             return false;
         }
     }
 
   function transferFrom(address _from, address _to, uint _value) returns (bool success) {
     var _allowance = allowed[_from][msg.sender];
-    // Check if we are not using SafeMath
     
-    if(_value > _allowance){
+    if(_value > _allowance) {
         throw;
     }
 
@@ -105,4 +127,4 @@ contract ERC23Token is ERC23 {
   function allowance(address _owner, address _spender) constant returns (uint remaining) {
     return allowed[_owner][_spender];
   }
-} 
+}
